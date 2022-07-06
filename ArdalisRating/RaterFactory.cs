2b7155714967880c3e09ae81ca1bda28f90371b3
Josip Pavic <Josip.Pavic@endava.com>
2022-07-06 16:44:39 +0200

    open-closed principle

---
+++ ArdalisRating/RaterFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class RaterFactory
+    {
+        public IRater Create(Policy policy, RatingEngine engine)
+        {
+            switch (policy.Type)
+            {
+                case PolicyType.Life:
+                    return new LifePolicyRater(engine, engine.logger);
+                case PolicyType.Land:
+                    return new LandPolicyRater(engine, engine.logger);
+                case PolicyType.Auto:
+                    return new AutoPolicyRater(engine, engine.logger);
+                default:
+                    return null;
+            }
+        }
+    }
+}
--- ArdalisRating/RatingEngine.cs
+++ ArdalisRating/RatingEngine.cs
@@ -14,7 +14,9 @@ namespace ArdalisRating
         public ConsoleLogger logger { get; set; } = new ConsoleLogger();
         public FilePolicySource policySource { get; set; } = new FilePolicySource();
         public PolicySerializer policySerializer { get; set; } = new PolicySerializer();
-        public decimal Rating { get; set; }
+
+        public decimal Rating;
+        
         public void Rate()
         {
             logger.Log("Starting rate.");
@@ -23,82 +25,12 @@ namespace ArdalisRating
 
             // load policy - open file policy.json
             string policyJson = policySource.GetPolicyFromSource();
+
             Policy policy = policySerializer.GetPolicyFromJsonString(policyJson);
 
-            switch (policy.Type)
-            {
-                case PolicyType.Auto:
-                    logger.Log("Rating AUTO policy...");
-                    logger.Log("Validating policy.");
-                    if (String.IsNullOrEmpty(policy.Make))
-                    {
-                        logger.Log("Auto policy must specify Make");
-                        return;
-                    }
-                    if (policy.Make == "BMW")
-                    {
-                        if (policy.Deductible < 500)
-                        {
-                            Rating = 1000m;
-                        }
-                        Rating = 900m;
-                    }
-                    break;
-
-                case PolicyType.Land:
-                    logger.Log("Rating LAND policy...");
-                    logger.Log("Validating policy.");
-                    if (policy.BondAmount == 0 || policy.Valuation == 0)
-                    {
-                        logger.Log("Land policy must specify Bond Amount and Valuation.");
-                        return;
-                    }
-                    if (policy.BondAmount < 0.8m * policy.Valuation)
-                    {
-                        logger.Log("Insufficient bond amount.");
-                        return;
-                    }
-                    Rating = policy.BondAmount * 0.05m;
-                    break;
-
-                case PolicyType.Life:
-                    logger.Log("Rating LIFE policy...");
-                    logger.Log("Validating policy.");
-                    if (policy.DateOfBirth == DateTime.MinValue)
-                    {
-                        logger.Log("Life policy must include Date of Birth.");
-                        return;
-                    }
-                    if (policy.DateOfBirth < DateTime.Today.AddYears(-100))
-                    {
-                        logger.Log("Centenarians are not eligible for coverage.");
-                        return;
-                    }
-                    if (policy.Amount == 0)
-                    {
-                        logger.Log("Life policy must include an Amount.");
-                        return;
-                    }
-                    int age = DateTime.Today.Year - policy.DateOfBirth.Year;
-                    if (policy.DateOfBirth.Month == DateTime.Today.Month &&
-                        DateTime.Today.Day < policy.DateOfBirth.Day ||
-                        DateTime.Today.Month < policy.DateOfBirth.Month)
-                    {
-                        age--;
-                    }
-                    decimal baseRate = policy.Amount * age / 200;
-                    if (policy.IsSmoker)
-                    {
-                        Rating = baseRate * 2;
-                        break;
-                    }
-                    Rating = baseRate;
-                    break;
-
-                default:
-                    logger.Log("Unknown policy type");
-                    break;
-            }
+            var raterFactory = new RaterFactory();
+            var rater = raterFactory.Create(policy, this);
+            rater.Rate(policy);
 
             logger.Log("Rating completed.");
         }
